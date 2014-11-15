84d0d5f9a05779366c5d6ed7497c780f898b55e7
Jonathan Chang <jonathan.chang13@gmail.com>
2014-11-14 19:49:27 -0800

    Adding Paige's template files

--- core/models.py
+++ core/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-	fullName = models.CharField(max_length=200);
+	fullName = models.CharField(max_length=200)
+
+class Review(models.Model):
+	course = models.ForeignKey(Course)
+	starsOverall = models.IntegerField(default=0)
